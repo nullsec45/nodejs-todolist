c985b476501f94e9e2d53e0781cbc3dfbcc0ad42
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-10-04 08:36:20 +0700

    Update and Delete Todo

--- todolist-service.mjs
+++ todolist-service.mjs
@@ -27,4 +27,26 @@ export class TodoListService{
             res.end();
         })
     }
+
+    updateTodo(req,res){
+        req.addListener("data", (data) => {
+            const body=JSON.parse(data.toString());
+            if(this.todoList[body.id]){
+                this.todoList[body.id]=body.todo;
+            }                   
+            res.write(this.getJsonTodoList());
+            res.end();
+        });
+    }
+
+    deleteTodo(req, res){
+        req.addListener("data", (data) => {
+            const body=JSON.parse(data.toString());
+            if(this.todoList[body.id]){
+                this.todoList.splice(body.id, 1);
+            }                   
+            res.write(this.getJsonTodoList());
+            res.end();
+        });
+    }
 }   
